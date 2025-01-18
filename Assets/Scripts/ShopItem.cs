bcfa3928ee48d60ac05bfbc7bca3fe3d72726bc8
SuperLan11 <92647630+SuperLan11@users.noreply.github.com>
2025-01-18 01:05:26 -0500

    more shop button functionality

--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -6,21 +6,23 @@ using TMPro;
 public class ShopItem : MonoBehaviour
 {
     [SerializeField] private int cost;
+    [SerializeField] private string upgradeText;
     private AudioSource buySfx;
 
     // Start is called before the first frame update
     void Start()
     {
         buySfx = GetComponent<AudioSource>();
+        transform.GetComponentInChildren<TextMeshProUGUI>().text = upgradeText + cost;
     }
 
     public void BuyItem()
-    {
-        Debug.Log("mouse down");
+    {        
         if (MoneyCounter.money >= cost)
         {
             MoneyCounter.SetMoney(MoneyCounter.money - cost);
-            transform.GetComponentInChildren<TextMeshProUGUI>().text = "";
+            cost += 5;
+            transform.GetComponentInChildren<TextMeshProUGUI>().text = upgradeText + cost;
             if(buySfx != null)
                 buySfx.Play();
         }
