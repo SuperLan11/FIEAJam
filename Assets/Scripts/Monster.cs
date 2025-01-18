2ec70fc49cb0ee5666b2b1840cd5ef55fbb1cf40
SuperLan11 <92647630+SuperLan11@users.noreply.github.com>
2025-01-17 23:28:32 -0500

    got basic monster line working and created credits file for sfx

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -5,6 +5,14 @@ public class Monster : MonoBehaviour
 	public bool isDragging;
 	public Vector2 dragDelta;
 	private Vector2 originalPos;
+
+	// variables added to move monsters in line
+	private bool isPlaced = false;
+	public bool isMoving = false;
+	Vector3 movePos = Vector3.zero;
+	
+	private static float moveAccel = 0.1f;
+	private static float monsterSpacing = 2f;
 	
 	private Vector2 GetMousePosition() => MainCamera.instance.camera.ScreenToWorldPoint(Input.mousePosition);
 	public void StartDrag()
@@ -24,12 +32,14 @@ public class Monster : MonoBehaviour
 			if (!display.isTargetable)
 			{
 				continue;
-			}
+			}			
 
 			Vector2 pos = display.GetSnapPosition(transform.position, out bool found);
 			if (found)
 			{
 				transform.position = pos;
+				isPlaced = true;
+				AdvanceLine();				
 				return;
 			}
 		}
@@ -38,11 +48,33 @@ public class Monster : MonoBehaviour
 
 	}
 
+	private void AdvanceLine()
+    {		
+		Monster[] monsters = FindObjectsOfType<Monster>();
+		foreach (Monster monster in monsters)
+		{
+			if (monster.isPlaced)
+				continue;
+			
+			monster.isMoving = true;
+			monster.movePos = monster.transform.position;
+			monster.movePos.x += monsterSpacing;
+		}
+	}
+
+
 	public void Update()
 	{
 		if (isDragging)
 		{
 			transform.position = GetMousePosition() + dragDelta;
 		}
-	}
+        else if (isMoving)
+        {            
+            transform.position = Vector3.Lerp(transform.position, movePos, moveAccel);
+
+            if (Vector3.Distance(transform.position, movePos) < 0.1f)
+                isMoving = false;
+        }
+    }
 }
