629f26fa8fd6721858d3946458f0f9f35645f2a1
SuperLan11 <92647630+SuperLan11@users.noreply.github.com>
2025-01-19 13:36:45 -0500

    latest bugged upgrade code

--- Assets/Scripts/DayPanel.cs
+++ Assets/Scripts/DayPanel.cs
@@ -62,6 +62,8 @@ public class DayPanel : MonoBehaviour
         StartCoroutine(RollMoneyResult(2, MoneyCounter.profit));
         MoneyCounter.profit = 0;
         StartCoroutine(WaitToLoad());
+
+        FindObjectOfType<GridDisplay>().ResetShape();
     }
 
     public IEnumerator RollMoneyResult(float rollTime, int profit)
