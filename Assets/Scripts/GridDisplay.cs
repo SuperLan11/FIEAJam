53065e67f934c9b5bd2686407c5e201753d4c2d4
SuperLan11 <92647630+SuperLan11@users.noreply.github.com>
2025-01-19 13:56:05 -0500

    hiding suprise box

--- Assets/Scripts/GridDisplay.cs
+++ Assets/Scripts/GridDisplay.cs
@@ -146,7 +146,7 @@ public class GridDisplay : MonoBehaviour
         shape = newShape;
         ResetShape();
 
-        LowerSquares();
+        //LowerSquares();
     }
 
     private void LowerSquares()
